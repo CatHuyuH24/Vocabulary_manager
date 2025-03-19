741c8b06d862c1b856d07e2ad0224ba5b7f590ce
Ma Cat Huynh <uynhhuc810@gmail.com>
2025-03-20 00:42:12 +0700

    init commit

---
+++ server/src/test/java/com/cloudintroduction/vocabulary_manager/VocabularyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudintroduction.vocabulary_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
