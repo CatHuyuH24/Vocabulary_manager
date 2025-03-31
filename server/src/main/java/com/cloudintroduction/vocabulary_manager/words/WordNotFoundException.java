66669c397860f3b8ef9077fdefdc38892e97fea8
Ma Cat Huynh <uynhhuc810@gmail.com>
2025-03-31 21:32:22 +0700

    update WordRepository
    - update WordNotFoundException to have String param
    - add delete word, update word functions
    - refine getWordById, getAllWords methods

--- server/src/main/java/com/cloudintroduction/vocabulary_manager/words/WordNotFoundException.java
+++ server/src/main/java/com/cloudintroduction/vocabulary_manager/words/WordNotFoundException.java
@@ -4,4 +4,8 @@ public class WordNotFoundException extends Exception {
     WordNotFoundException() {
         super("Word not found");
     }
+
+    WordNotFoundException(String message) {
+        super(message);
+    }
 }
